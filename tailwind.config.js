84634d820fd114778d2d9a32e17a82d4f8f8ab6c
Prabhash <besttlookk@gmail.com>
2022-03-10 16:10:14 +0530

    v1

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  purge: ["./src/**/*.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      backgroundImage: {
+        hero: "url('../img/pic2.jpg')",
+        help: "url('../img/pic3.jpg')",
+        about: "url('../img/about.jpg')",
+        contact: "url('../img/contact.jpg')",
+      },
+
+      fontFamily: {
+        sansita: ["Sansita", "sans-serif"],
+      },
+
+      transitionDelay: {
+        2000: "2000ms",
+      },
+
+      height: {
+        "vh-7": "70vh",
+
+        "vh-8": "80vh",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
